72526df2d4283dbad0b1067bd6582bec4e725a4f
abhishyam <chabhishyam@gmail.com>
2017-03-26 11:52:53 +0530

    added Test case 2 for bfs traversal

--- src/com/examples/TestBfsOfGraph.java
+++ src/com/examples/TestBfsOfGraph.java
@@ -6,6 +6,7 @@ import com.examples.graph.Graph;
 public class TestBfsOfGraph {
 
     public static void main(String[] args) {
+        //Test Case 1
         Graph<Integer> graph= new AdjacencyListGraphImpl<>();
         graph.addEdge(1,2); graph.addEdge(1,3); graph.addEdge(1,4);
         graph.addEdge(2,5);graph.addEdge(2,6);
@@ -13,7 +14,19 @@ public class TestBfsOfGraph {
         graph.addEdge(5,9); graph.addEdge(5,10);
         graph.addEdge(7,11); graph.addEdge(7,12);
         System.out.println(graph);
-        System.out.println("BFS Of Graph");
+        System.out.println("BFS Of Graph for Test Case 1");
         System.out.println(graph.bfs(1).toString());
+        //Test Case 2
+        System.out.println("-------------------------------------------------------");
+        Graph<Integer> graph1 = new AdjacencyListGraphImpl<>();
+        graph1.addEdge(0, 1);
+        graph1.addEdge(0, 2);
+        graph1.addEdge(1, 2);
+        graph1.addEdge(2, 0);
+        graph1.addEdge(2, 3);
+        graph1.addEdge(3, 3);
+        System.out.println(graph1);
+        System.out.println("BFS Of Graph for Test Case 2");
+        System.out.println(graph1.bfs(2).toString());
     }
 }
